dc3ad63d03b4009b0cd26141a44ddce2248dea8a
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-29 11:01:25 -0400

    added forms so you can add a City on the html page

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8411709dfd1a3f1f2309d18434e9b26'
-    city = 'Columbus'
+
+
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -28,5 +35,5 @@ def index(request):
 
 
     print(weather_data)
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form' : form}
     return render(request, 'weather/weather.html', context)
