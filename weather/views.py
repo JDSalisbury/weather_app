a4658b9889fac89c69cc0f4575a6102c21ee59b4
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-29 09:19:21 -0400

    misspelled return

--- weather/views.py
+++ weather/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    retun render(request, 'weather/weather.html')
+    return render(request, 'weather/base.html')
