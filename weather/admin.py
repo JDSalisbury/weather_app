96d157271efd6606be11be60884cdf57b5f0f730
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-29 10:38:09 -0400

    City is an object instead of the name of the city, something to change. but what is happening is now my request can cycle through a list of given cities and return the info for each one with the for loop.

--- weather/admin.py
+++ weather/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import City
 # Register your models here.
 
-admins.site.register(City)
+admin.site.register(City)
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 
 # Create your views here.
@@ -7,16 +8,25 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8411709dfd1a3f1f2309d18434e9b26'
     city = 'Columbus'
 
-    r = requests.get(url.format(city)).json()
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
 
 
-    city_weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
-    }
 
-    print(city_weather)
+    print(weather_data)
     context = {'city_weather': city_weather}
     return render(request, 'weather/weather.html', context)
