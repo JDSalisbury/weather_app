efd499bd0bc7da5879beb7515c29a0ec25d8a4a8
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-29 09:51:23 -0400

    setting up the api info

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/base.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8411709dfd1a3f1f2309d18434e9b26'
+    city = 'Columbus'
+
+    r = request.get(url.format(city))
+    print(r.text)
+
+    return render(request, 'weather/weather.html')
